07cae0fda5ee6cd2228fd189058e9cfafc7d79ba
MarkSuttirak <suttirak.ch@zaviago.com>
2023-05-27 13:55:32 +0700

    Commit Codes

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -4,14 +4,52 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 
 const Hero = () => {
-  const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
+  const incentives = [
+    {
+      name: 'สินค้าดีที่เราแนะนำ',
+      description: "It's not actually free we just price it into the products. Someone's paying for it, and it's not us.",
+      imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-delivery-light.svg',
+    },
+    {
+      name: 'ราคาดีการันตี',
+      description: 'Our AI chat widget is powered by a naive series of if/else statements. Guaranteed to irritate.',
+      imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-chat-light.svg',
+    },
+    {
+      name: 'โค้ดและโปรโมชั่น',
+      description: "Look how fast that cart is going. What does this mean for the actual experience? I don't know.",
+      imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-fast-checkout-light.svg',
+    },
+    {
+      name: 'ตัวอย่างสินค้า',
+      description: "Buy them for your friends, especially if they don't like our store. Free money for us, it's great.",
+      imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-gift-card-light.svg',
+    },
+    {
+      name: 'สินค้านี้จัดส่งฟรี',
+      description: "Buy them for your friends, especially if they don't like our store. Free money for us, it's great.",
+      imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-gift-card-light.svg',
+    },
   ]
 
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const guarantees = [
+    {
+      name: 'สินค้าคุณภาพ',
+      description: "สินค้าผลิตจากวัสดุที่มีคุณภาพ และได้มาตรฐาน CE",
+      imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-delivery-light.svg',
+    },
+    {
+      name: 'รับประกัน 1 ปี',
+      description: 'รับประกันสินค้าสูงสุด 1 ปี',
+      imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-chat-light.svg',
+    },
+    {
+      name: 'บริการ',
+      description: "จัดส่งรวดเร็ว พร้อมดูแลสินค้าตลอดอายุการใช้งาน",
+      imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-fast-checkout-light.svg',
+    },
+  ]
+
   return (
     <>
     <div className="w-full relative content-container pt-15">
@@ -35,142 +73,28 @@ const Hero = () => {
       </div>
     </div>
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-                {item.name}
-              </a>
-            ))}
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
-        </nav>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-              </button>
+      <div className="mx-auto max-w-2xl py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-5 lg:gap-x-8">
+          {incentives.map((incentive) => (
+            <div key={incentive.name}>
+              <img src={incentive.imageSrc} alt="" className="h-24 w-auto" />
+              <h3 className="mt-6 text-sm font-medium text-black">{incentive.name}</h3>
+              <p className="mt-2 text-sm text-black">{incentive.description}</p>
             </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
-      </header>
-
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
+          ))}
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
+      </div>
+    </div>
+    <div className="bg-red-600">
+      <div className="mx-auto max-w-2xl py-24 px-4 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8">
+          {guarantees.map((guarantee) => (
+            <div key={guarantee.name}>
+              <img src={guarantee.imageSrc} alt="" className="h-24 w-auto" />
+              <h3 className="mt-6 text-xl font-medium text-white">{guarantee.name}</h3>
+              <p className="mt-2 text-sm text-white">{guarantee.description}</p>
             </div>
-          </div>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
+          ))}
         </div>
       </div>
     </div>
