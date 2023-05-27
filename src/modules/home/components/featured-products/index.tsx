5c0ee7bdc843a7b1d6c4b1cc19f6e94b0301526c
MarkSuttirak <suttirak.ch@zaviago.com>
2023-05-27 15:21:43 +0700

    Update index.tsx

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -6,6 +6,63 @@ import SkeletonProductPreview from "@modules/skeletons/components/skeleton-produ
 const FeaturedProducts = () => {
   const { data } = useFeaturedProductsQuery()
 
+  const posts = [
+    {
+      title: 'Boost your conversion rate',
+      href: '#',
+      category: { name: 'Article', href: '#' },
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+      date: 'Mar 16, 2020',
+      datetime: '2020-03-16',
+      imageUrl:
+        'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+      readingTime: '6 min',
+      author: {
+        name: 'Roel Aufderehar',
+        href: '#',
+        imageUrl:
+          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    },
+    {
+      title: 'How to use search engine optimization to drive sales',
+      href: '#',
+      category: { name: 'Video', href: '#' },
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
+      date: 'Mar 10, 2020',
+      datetime: '2020-03-10',
+      imageUrl:
+        'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+      readingTime: '4 min',
+      author: {
+        name: 'Brenna Goyette',
+        href: '#',
+        imageUrl:
+          'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    },
+    {
+      title: 'Improve your customer experience',
+      href: '#',
+      category: { name: 'Case Study', href: '#' },
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
+      date: 'Feb 12, 2020',
+      datetime: '2020-02-12',
+      imageUrl:
+        'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+      readingTime: '11 min',
+      author: {
+        name: 'Daniela Metz',
+        href: '#',
+        imageUrl:
+          'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    },
+  ]
+
   const features = [
     {
       name: 'เครื่องมือช่างไร้สาย',
@@ -154,7 +211,7 @@ const FeaturedProducts = () => {
       <div className="mx-auto max-w-2xl py-24 px-4 sm:py-32 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="block">
           <div className="inline-block w-1/2">
-            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">ประเภทสินค้า</p>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-black sm:text-4xl">ประเภทสินค้า</p>
           </div>
           <div className="inline-block w-1/2 text-right">
             <a href="#" className="text-base font-semibold leading-7 text-red-600">
@@ -167,7 +224,7 @@ const FeaturedProducts = () => {
           {features.map((feature) => (
             <div key={feature.name} className="flex flex-col-reverse">
               <div className="mt-6">
-                <h3 className="text-sm font-medium text-gray-900">{feature.name}</h3>
+                <h3 className="text-xl font-medium text-center text-black">{feature.name}</h3>
               </div>
               <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-100">
                 <img src={feature.imageSrc} alt={feature.imageAlt} className="object-cover object-center" />
@@ -178,32 +235,271 @@ const FeaturedProducts = () => {
       </div>
     </div>
 
-    <div className="py-12">
-      <div className="content-container py-12">
-        <div className="flex flex-col items-center text-center mb-16">
-          <span className="text-base-regular text-gray-600 mb-6">
-            Latest products
-          </span>
-          <p className="text-2xl-regular text-gray-900 max-w-lg mb-4">
-            Our newest styles are here to help you look your best.
-          </p>
-          <UnderlineLink href="/store">Explore products</UnderlineLink>
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl py-24 px-4 sm:py-32 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="block">
+          <div className="inline-block w-1/2">
+            <p className="mt-2 text-3xl font-bold tracking-tight text-black sm:text-4xl">สินค้าโปรโมชั่น</p>
+          </div>
+          <div className="inline-block w-1/2 text-right">
+            <a href="#" className="text-base font-semibold leading-7 text-red-600">
+              ดูสินค้าทั้งหมด <span aria-hidden="true">→</span>
+            </a>
+          </div>
         </div>
-        <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-4 gap-y-8">
+
+        <div className="flex" aria-label="Breadcrumb">
+          <ol role="list" className="flex items-center space-x-4 mt-6">
+            <li>
+              <div className="flex items-center">
+                <a
+                  href="#"
+                  className="text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                  เครื่องมือช่าง
+                </a>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-gray-300"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  aria-hidden="true"
+                >
+                  <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                </svg>
+                <a
+                  href="#"
+                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                  ปั๊มน้ำ
+                </a>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-gray-300"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  aria-hidden="true"
+                >
+                  <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                </svg>
+                <a
+                  href="#"
+                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                  เครื่องฉีดน้ำ
+                </a>
+              </div>
+            </li>
+          </ol>
+        </div>
+
+        <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-4 gap-y-8 mt-8">
           {data
             ? data.map((product) => (
                 <li key={product.id}>
                   <ProductPreview {...product} />
                 </li>
               ))
-            : Array.from(Array(8).keys()).map((i) => (
+            : Array.from(Array(4).keys()).map((i) => (
                 <li key={i}>
                   <SkeletonProductPreview />
                 </li>
               ))}
-        </ul>
+          </ul>
+        </div>
       </div>
-    </div>
+
+      <div className="bg-white">
+      <div className="mx-auto max-w-2xl py-24 px-4 sm:py-32 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="block">
+          <div className="inline-block w-1/2">
+            <p className="mt-2 text-3xl font-bold tracking-tight text-black sm:text-4xl">สินค้าแนะนำ</p>
+          </div>
+          <div className="inline-block w-1/2 text-right">
+            <a href="#" className="text-base font-semibold leading-7 text-red-600">
+              ดูสินค้าทั้งหมด <span aria-hidden="true">→</span>
+            </a>
+          </div>
+        </div>
+
+        <div className="flex" aria-label="Breadcrumb">
+          <ol role="list" className="flex items-center space-x-4 mt-6">
+            <li>
+              <div className="flex items-center">
+                <a
+                  href="#"
+                  className="text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                  ปั๊มน้ำ
+                </a>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-gray-300"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  aria-hidden="true"
+                >
+                  <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                </svg>
+                <a
+                  href="#"
+                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                  เครื่องมือไร้สาย
+                </a>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-gray-300"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  aria-hidden="true"
+                >
+                  <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                </svg>
+                <a
+                  href="#"
+                  className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                  สินค้าใหม่
+                </a>
+              </div>
+            </li>
+          </ol>
+        </div>
+
+        <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-4 gap-y-8 mt-8">
+          {data
+            ? data.map((product) => (
+                <li key={product.id}>
+                  <ProductPreview {...product} />
+                </li>
+              ))
+            : Array.from(Array(4).keys()).map((i) => (
+                <li key={i}>
+                  <SkeletonProductPreview />
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+
+      {/* E-Catalog */}
+      <div className="bg-white">
+        <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+          <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Boost your productivity.
+                <br />
+                Start using our app today.
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                <a
+                  href="#"
+                  className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                >
+                  Get started
+                </a>
+                <a href="#" className="text-base font-semibold leading-7 text-white">
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <img
+                className="absolute top-0 left-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                alt="App screenshot"
+                width={1824}
+                height={1080}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Blog Section */}
+      <div className="relative bg-gray-50 px-6 pt-16 pb-20 lg:px-8 lg:pt-24 lg:pb-28">
+        <div className="absolute inset-0">
+          <div className="h-1/3 bg-white sm:h-2/3" />
+        </div>
+        <div className="relative mx-auto max-w-7xl">
+          <div className="block">
+            <div className="text-left w-1/2 inline-block">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">บทความ และ ข่าวสาร</h2>
+              <p className="mx-auto mt-3 text-xl text-gray-500 sm:mt-4">
+                บทความน่ารู้ และสาระต่าง ๆ เกี่ยวกับปั๊มน้ำ และเครื่องมือช่าง
+              </p>
+            </div>
+            <div className="text-right w-1/2 inline-block">
+              <a href="#" className="text-base font-semibold leading-7 text-red-600">
+                ดูบทความ และ ข่าวสารทั้งหมด <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+          <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
+            {posts.map((post) => (
+              <div key={post.title} className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                <div className="flex-shrink-0">
+                  <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
+                </div>
+                <div className="flex flex-1 flex-col justify-between bg-white p-6">
+                  <div className="flex-1">
+                    <p className="text-sm font-medium text-indigo-600">
+                      <a href={post.category.href} className="hover:underline">
+                        {post.category.name}
+                      </a>
+                    </p>
+                    <a href={post.href} className="mt-2 block">
+                      <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                      <p className="mt-3 text-base text-gray-500">{post.description}</p>
+                    </a>
+                  </div>
+                  <div className="mt-6 flex items-center">
+                    <div className="flex-shrink-0">
+                      <a href={post.author.href}>
+                        <span className="sr-only">{post.author.name}</span>
+                        <img className="h-10 w-10 rounded-full" src={post.author.imageUrl} alt="" />
+                      </a>
+                    </div>
+                    <div className="ml-3">
+                      <p className="text-sm font-medium text-gray-900">
+                        <a href={post.author.href} className="hover:underline">
+                          {post.author.name}
+                        </a>
+                      </p>
+                      <div className="flex space-x-1 text-sm text-gray-500">
+                        <time dateTime={post.datetime}>{post.date}</time>
+                        <span aria-hidden="true">&middot;</span>
+                        <span>{post.readingTime} read</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
